f80f9b5f674d351829f928fcb88cbcfb3dc3ad67
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-04 16:45:42 +0700

    added logout api slice

--- client/src/features/auth/authApiSlice.js
+++ client/src/features/auth/authApiSlice.js
@@ -1,4 +1,5 @@
 import { baseApiSlice } from "../api/baseApiSlice";
+import { logOut } from "./authSlice";
 
 export const authApiSlice = baseApiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -15,8 +16,22 @@ export const authApiSlice = baseApiSlice.injectEndpoints({
                 method: 'POST',
                 body: credentials
             }),
-        })
+        }),
+        logoutUser: builder.mutation({
+            query: () => ({
+                url: '/auth/logout',
+                method: 'POST',
+            })
+        }),
+        async onQueryStarted(args, { dispatch, queryFulfilled }) {
+            try {
+                dispatch(logOut())
+                dispatch(baseApiSlice.util.resetApiState)
+            } catch (error) {
+                console.log(error);
+            }
+        }
     })
 })
 
-export const { useRegisterUserMutation, useLoginUserMutation } = authApiSlice
+export const { useRegisterUserMutation, useLoginUserMutation, useLogoutUserMutation } = authApiSlice
