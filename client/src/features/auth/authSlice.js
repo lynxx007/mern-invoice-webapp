f122b2a9df18c3a0d8bb973ad2c32dd26981a740
lynxx007 <luthfirizky7@gmail.com>
2023-11-23 15:21:33 +0700

    added googleToken state

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,28 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { decodeToken } from "react-jwt";
 
-const user = JSON.parse(localStorage.getItem('user'))
+const user = JSON.parse(localStorage.getItem("user"));
+const googleToken = localStorage.getItem("googleToken");
+
+const decodedGoogleToken = decodeToken(googleToken);
 
 const initialState = {
-    user: user ? user : null,
-}
+  user: user ? user : decodedGoogleToken,
+  googleToken: googleToken ? googleToken : null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        logIn: (state, action) => {
-            state.user = action.payload
-            localStorage.setItem('user', JSON.stringify(action.payload))
-        },
-        logOut: (state) => {
-            state.user - null
-            localStorage.removeItem('user')
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    logIn: (state, action) => {
+      state.user = action.payload;
+      localStorage.setItem("user", JSON.stringify(action.payload));
+    },
+    logOut: (state, action) => {
+      state.user = null;
+      state.googleToken = null;
+      localStorage.removeItem("user");
+      localStorage.removeItem("googleToken");
+    },
+  },
+});
 
-export const { logIn, logOut } = authSlice.actions
+export const { logIn, logOut } = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
 
-export const selectCurrentUserToken = state => state.auth.user?.accessToken
+export const selectCurrentUserToken = (state) => state.auth.user?.accessToken;
+export const selectCurrentUserGoogleToken = (state) => state.auth?.googleToken;
