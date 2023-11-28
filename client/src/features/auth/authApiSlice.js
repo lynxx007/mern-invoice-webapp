45551037bdd7e2905fe56ee253bd5a5ce2694f29
lynxx007 <luthfirizky7@gmail.com>
2023-11-28 20:54:32 +0700

    added resend verify email, reset password mutation

--- client/src/features/auth/authApiSlice.js
+++ client/src/features/auth/authApiSlice.js
@@ -2,36 +2,64 @@ import { baseApiSlice } from "../api/baseApiSlice";
 import { logOut } from "./authSlice";
 
 export const authApiSlice = baseApiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        registerUser: builder.mutation({
-            query: (userData) => ({
-                url: '/auth/register',
-                method: 'POST',
-                body: userData
-            })
-        }),
-        loginUser: builder.mutation({
-            query: (credentials) => ({
-                url: '/auth/login',
-                method: 'POST',
-                body: credentials
-            }),
-        }),
-        logoutUser: builder.mutation({
-            query: () => ({
-                url: '/auth/logout',
-                method: 'POST',
-            })
-        }),
-        async onQueryStarted(args, { dispatch, queryFulfilled }) {
-            try {
-                dispatch(logOut())
-                dispatch(baseApiSlice.util.resetApiState)
-            } catch (error) {
-                console.log(error);
-            }
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (userData) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    logoutUser: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        try {
+          dispatch(logOut());
+          dispatch(baseApiSlice.util.resetApiState);
+        } catch (error) {
+          console.log(error);
         }
-    })
-})
+      },
+    }),
+    resendVerifyEmail: builder.mutation({
+      query: (email) => ({
+        url: "/auth/resend_email_token",
+        method: "POST",
+        body: email,
+      }),
+    }),
+    resetPasswordRequest: builder.mutation({
+      query: (formData) => ({
+        url: "/auth/reset_password_request",
+        method: "POST",
+        body: formData,
+      }),
+    }),
+    resetPassword: builder.mutation({
+      query: (formData) => ({
+        url: "/auth/reset_password",
+        method: "POST",
+        body: formData,
+      }),
+    }),
+  }),
+});
 
-export const { useRegisterUserMutation, useLoginUserMutation, useLogoutUserMutation } = authApiSlice
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation,
+  useResendVerifyEmailMutation,
+  useResetPasswordMutation,
+  useResetPasswordRequestMutation,
+} = authApiSlice;
