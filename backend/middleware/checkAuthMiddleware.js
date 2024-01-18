6f088eb45c9991c2e42e212dde14ac9bcc721204
lynxx007 <luthfirizky7@gmail.com>
2024-01-18 10:45:18 +0700

    refractor

--- backend/middleware/checkAuthMiddleware.js
+++ backend/middleware/checkAuthMiddleware.js
@@ -1,30 +1,30 @@
-import asyncHandler from 'express-async-handler';
-import jwt from 'jsonwebtoken'
-import User from '../models/userModel.js';
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
 const checkAuth = asyncHandler(async (req, res, next) => {
-    let jwt_token
+  let jwt_token;
 
-    const authHeader = req.headers.authorization || req.headers.Authorization
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401)
+  if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401);
 
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        jwt_token = authHeader.split(" ")[1]
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    jwt_token = authHeader.split(" ")[1];
 
-        jwt.verify(
-            jwt_token,
-            process.env.JWT_ACCESS_SECRET_KEY,
-            async (error, decoded) => {
-                if (error) return res.sendStatus(403)
+    jwt.verify(
+      jwt_token,
+      process.env.JWT_ACCESS_SECRET_KEY,
+      async (error, decoded) => {
+        if (error) return res.sendStatus(403);
 
-                const userId = decoded.id;
-                req.user = await User.findById(userId).select("-password")
-                req.roles = decoded.roles
-                next()
-            })
+        const userId = decoded.id;
+        req.user = await User.findById(userId).select("-password");
 
-    }
-})
+        next();
+      }
+    );
+  }
+});
 
-export default checkAuth
+export default checkAuth;
