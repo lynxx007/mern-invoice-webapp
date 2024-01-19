624bd49d1b76b5b813d2072080f5fb0b19d307f3
lynxx007 <luthfirizky7@gmail.com>
2024-01-19 13:29:07 +0700

    added google token decoder

--- client/src/hooks/useAuthUser.jsx
+++ client/src/hooks/useAuthUser.jsx
@@ -1,23 +1,34 @@
-import { decodeToken } from 'react-jwt'
-import { useSelector } from 'react-redux'
-import { selectCurrentUserToken } from '../features/auth/authSlice'
-
+import { decodeToken } from "react-jwt";
+import { useSelector } from "react-redux";
+import {
+  selectCurrentUserToken,
+  selectCurrentUserGoogleToken,
+} from "../features/auth/authSlice";
 
 export const useAuthUser = () => {
-    const token = useSelector(selectCurrentUserToken)
+  const token = useSelector(selectCurrentUserToken);
+  const googleToken = useSelector(selectCurrentUserGoogleToken);
 
-    let isAdmin = false
-    let accessRight = 'User'
+  let isAdmin = false;
+  let accessRight = "User";
 
-    if (token) {
-        const decodedToken = decodeToken(token)
-        const { roles } = decodedToken
+  if (token) {
+    const decodedToken = decodeToken(token);
+    const { roles } = decodedToken;
 
-        isAdmin = roles.includes('Admin')
+    isAdmin = roles.includes("Admin");
+    if (isAdmin) accessRight = "Admin";
 
-        if (isAdmin) accessRight = 'Admin'
-        return { roles, isAdmin, accessRight }
-    }
+    return { roles, isAdmin, accessRight };
+  } else if (googleToken) {
+    const googleDecodedToken = decodeToken(googleToken);
+    const { roles } = googleDecodedToken;
 
-    return { roles: [], isAdmin, accessRight }
-}
+    isAdmin = roles.includes("Admin");
+    if (isAdmin) accessRight = "Admin";
+
+    return { roles, isAdmin, accessRight };
+  }
+
+  return { roles: [], isAdmin, accessRight };
+};
