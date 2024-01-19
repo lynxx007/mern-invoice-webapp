185b5e662f08cc52f1078b22062746e3d694e478
lynxx007 <luthfirizky7@gmail.com>
2024-01-19 13:27:07 +0700

    added passport initialize

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,64 @@
-import express from 'express'
-import chalk from 'chalk'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import mongoSanitize from 'express-mongo-sanitize'
-import "dotenv/config.js"
+import express from "express";
+import chalk from "chalk";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import "dotenv/config.js";
 
-import { connectDb } from './config/connectDb.js'
-import { morganMiddleware, systemLogs } from './utils/Logger.js'
-import { errorHandler, notFound } from '../backend/middleware/errorMiddleware.js'
+import { connectDb } from "./config/connectDb.js";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
+import {
+  errorHandler,
+  notFound,
+} from "../backend/middleware/errorMiddleware.js";
 
-import authRoutes from './routes/authRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import { apiLimiter } from './middleware/apiLimiter.js'
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { apiLimiter } from "./middleware/apiLimiter.js";
+import passport from "passport";
+import { googleAuth } from "./config/passportSetup.js";
 
-await connectDb()
+await connectDb();
 
-const app = express()
+const app = express();
 
-app.set('trust proxy', '127.0.0.1')
+app.set("trust proxy", "127.0.0.1");
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
+app.use(passport.initialize());
+googleAuth();
 
-app.use(mongoSanitize())
+app.use(cookieParser());
 
-app.use(morganMiddleware)
+app.use(mongoSanitize());
 
-app.get('/api/v1/test', (req, res) => {
-    res.send('hello world')
-})
+app.use(morganMiddleware);
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/user', apiLimiter, userRoutes)
+app.get("/api/v1/test", (req, res) => {
+  res.send("hello world");
+});
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", apiLimiter, userRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(
-        `${chalk.green.bold('👍')} Server is running in ${process.env.NODE_ENV} on port ${chalk.blue.bold(PORT)}`
-    );
-    systemLogs.info(`Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
-})
+  console.log(
+    `${chalk.green.bold("👍")} Server is running in ${
+      process.env.NODE_ENV
+    } on port ${chalk.blue.bold(PORT)}`
+  );
+  systemLogs.info(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
+});
